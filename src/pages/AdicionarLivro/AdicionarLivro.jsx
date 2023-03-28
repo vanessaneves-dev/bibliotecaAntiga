70abebb6177e03b60b2089f9c015182cc367404d
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-28 13:51:52 -0300

    Formulario adicionar livro com validação

--- src/pages/AdicionarLivro/AdicionarLivro.jsx
+++ src/pages/AdicionarLivro/AdicionarLivro.jsx
@@ -18,37 +18,37 @@ export function AdicionarLivro() {
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Título</Form.Label>
-                        <Form.Control type="text" {...register("titulo", {required:true, maxLength: 255})} />
-                        <Form.Text className="text-muted">
-                            {/* messagem de validação */}
+                        <Form.Control type="text" className={errors.titulo && "is-invalid"} {...register("titulo", {required: "Título é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
+                        <Form.Text className="text-danger">
+                            {errors.titulo?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Autor</Form.Label>
-                        <Form.Control type="text" {...register("autor", {required:true, maxLength: 255})} />
-                        <Form.Text className="text-muted">
-                            {/* messagem de validação */}
+                        <Form.Control type="text" className={errors.autor && "is-invalid"} {...register("autor", {required: "Autor é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
+                        <Form.Text className="text-danger">
+                            {errors.autor?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Categoria</Form.Label>
-                        <Form.Control type="text" {...register("categoria", {required:true, maxLength: 255})} />
-                        <Form.Text className="text-muted">
-                            {/* messagem de validação */}
+                        <Form.Control type="text" className={errors.categoria && "is-invalid"} {...register("categoria", {required: "Categoria é obrigatória!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
+                        <Form.Text className="text-danger">
+                            {errors.categoria?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>ISBN</Form.Label>
-                        <Form.Control type="text" {...register("isbn", {required:true, maxLength: 255})} />
-                        <Form.Text className="text-muted">
-                            {/* messagem de validação */}
+                        <Form.Control type="text" className={errors.isbn && "is-invalid"} {...register("isbn", {required: "ISBN é obrigatório!", maxLength: {value: 255, message: "Limite de 255 caracteres!"}})} />
+                        <Form.Text className="text-danger">
+                            {errors.isbn?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Imagem da capa</Form.Label>
-                        <Form.Control type="url" {...register("urlCapa", {required:true})} />
-                        <Form.Text className="text-muted">
-                            {/* messagem de validação */}
+                        <Form.Control type="url" className={errors.urlCapa && "is-invalid"} {...register("urlCapa", {required: "O endereço da capa é obrigatório!"})} />
+                        <Form.Text className="text-danger">
+                            {errors.urlCapa?.message}
                         </Form.Text>
                     </Form.Group>
                     <Button type="submit" variant="success">Adicionar</Button>
