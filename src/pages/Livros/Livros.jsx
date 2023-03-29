b4ee05cfc56c3771385d02418e529f463043f7ba
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-28 22:20:46 -0300

    Loader

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Loader } from "../../components/Loader/Loader";
 import { getLivros } from "../../firebase/livros";
+import "./Livros.css";
 
 export function Livros() {
 
-    const [livros, setLivros] = useState([]);
+    const [livros, setLivros] = useState(null);
 
-    useEffect(async () => {
-        const busca = await getLivros();
-        setLivros(busca)
+    useEffect(() => {
+        getLivros().then(busca => {
+            setLivros(busca) 
+        })
     }, []);
 
     return (
@@ -22,32 +25,36 @@ export function Livros() {
                     </Button>
                 </div>
                 <hr />
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th>Autor</th>
-                            <th>Categoria</th>
-                            <th>ISBN</th>
-                            <th>Imagem</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {livros.map(livro => {
-                            return (
-                                <tr>
-                                    <td>{livro.titulo}</td>
-                                    <td>{livro.autor}</td>
-                                    <td>{livro.categoria}</td>
-                                    <td>{livro.isbn}</td>
-                                    <td>
-                                        <img src={livro.urlCapa} alt={livro.titulo} />
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
+                {livros === null ?
+                    <Loader />
+                    : 
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Título</th>
+                                <th>Autor</th>
+                                <th>Categoria</th>
+                                <th>ISBN</th>
+                                <th>Imagem</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {livros.map(livro => {
+                                return (
+                                    <tr key={livro.id}>
+                                        <td>{livro.titulo}</td>
+                                        <td>{livro.autor}</td>
+                                        <td>{livro.categoria}</td>
+                                        <td>{livro.isbn}</td>
+                                        <td>
+                                            <img src={livro.urlCapa} alt={livro.titulo} />
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                }
             </Container>
         </div>
     )
