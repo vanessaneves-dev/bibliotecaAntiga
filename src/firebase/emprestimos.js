370d2c3f0ea63f4dc191b99b6a87196232065926
gabriel-soulcode <gabriel.braga@soulcode.com>
2023-03-29 17:22:16 -0300

    Tabela de emprestimos

--- src/firebase/emprestimos.js
+++ src/firebase/emprestimos.js
@@ -1,6 +1,15 @@
-import { addDoc } from "firebase/firestore";
+import { addDoc, getDocs } from "firebase/firestore";
 import { emprestimosCollection } from "./collections";
 
 export async function adicionarEmprestimo(data) {
     await addDoc(emprestimosCollection, data);
+}
+
+export async function getEmprestimos() {
+    const snapshot = await getDocs(emprestimosCollection);
+    let emprestimos = [];
+    snapshot.forEach(doc => {
+        emprestimos.push({...doc.data(), id: doc.id});
+    });
+    return emprestimos;
 }
--- src/pages/Emprestimos/Emprestimos.jsx
+++ src/pages/Emprestimos/Emprestimos.jsx
@@ -1,7 +1,19 @@
-import { Button, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Badge, Button, Container, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { getEmprestimos } from "../../firebase/emprestimos";
+import { Loader } from "../../components/Loader/Loader";
 
 export function Emprestimos() {
+
+    const [emprestimos, setEmprestimos] = useState(null);
+
+    useEffect(() => {
+        getEmprestimos().then(busca => {
+            setEmprestimos(busca);
+        })
+    }, [])
+
     return (
         <div className="emprestimos">
             <Container>
@@ -10,6 +22,41 @@ export function Emprestimos() {
                     <Button as={Link} to="/emprestimos/adicionar" variant="success">Adicionar emprestimo</Button>
                 </div>
                 <hr />
+                {
+                    emprestimos === null ?
+                        <Loader />
+                        :
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>Leitor</th>
+                                    <th>E-mail</th>
+                                    <th>Telefone</th>
+                                    <th>Livro</th>
+                                    <th>Status</th>
+                                    <th>Data de Empréstimo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {emprestimos.map(emprestimo => {
+                                    const dataEmprestimo = emprestimo.dataEmprestimo.toDate().toLocaleDateString('pt-br');
+                                    return (
+                                        <tr>
+                                            <td>{emprestimo.leitor}</td>
+                                            <td>{emprestimo.email}</td>
+                                            <td>{emprestimo.telefone}</td>
+                                            <td>{emprestimo.livro.titulo}</td>
+                                            <td>
+                                                <Badge bg={emprestimo.status === "Pendente" ? "warning" : "success"}>{emprestimo.status}</Badge>
+                                            </td>
+                                            <td>{dataEmprestimo}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                }
+
             </Container>
         </div>
     )
