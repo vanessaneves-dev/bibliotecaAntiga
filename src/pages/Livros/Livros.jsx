8f9ae87008d455f19d24eba93698f33fd710223a
daydocx <contato.dayseguedes@gmail.com>
2023-04-04 16:35:45 -0300

    criação e implementação do dark mood

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -5,89 +5,107 @@ import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
 import { deleteLivro, getLivros } from "../../firebase/livros";
 import "./Livros.css";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 export function Livros() {
+  const [livros, setLivros] = useState(null);
 
-    const [livros, setLivros] = useState(null);
+  useEffect(() => {
+    initializeTable();
+  }, []);
 
-    useEffect(() => {
+  function initializeTable() {
+    getLivros().then((resultados) => {
+      setLivros(resultados);
+    });
+  }
+
+  function onDeleteLivro(id, titulo) {
+    const deletar = window.confirm(
+      `Tem certeza que deseja excluir o livro ${titulo}?`
+    );
+    if (deletar) {
+      deleteLivro(id).then(() => {
+        toast.success(`${titulo} apagado com sucesso!`, {
+          duration: 2000,
+          position: "bottom-right",
+        });
         initializeTable();
-    }, []);
-
-    function initializeTable() {
-        getLivros().then(resultados => {
-            setLivros(resultados)
-        })
+      });
     }
+  }
+  const resultado = useContext(ThemeContext);
+  const temaEscuro = resultado.temaEscuro;
 
-    function onDeleteLivro(id, titulo) {
-        const deletar = window.confirm(`Tem certeza que deseja excluir o livro ${titulo}?`);
-        if(deletar) {
-            deleteLivro(id).then(() => {
-                toast.success(`${titulo} apagado com sucesso!`, {duration: 2000, position: "bottom-right"});
-                initializeTable();
-            })
-        }
-    }
-
-    return (
-        <div className="livros">
-            <Container>
-                <div className="d-flex justify-content-between align-items-center">
-                    <h1>Livros</h1>
-                    <Button as={Link} to="/livros/adicionar" style={{ backgroundColor: "#248dad" }}>
-                        Adicionar Livro
-                    </Button>
-                </div>
-                <hr />
-                {livros === null ?
-                    <Loader />
-                    : 
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Título</th>
-                                <th>Autor</th>
-                                <th>Categoria</th>
-                                <th>ISBN</th>
-                                <th>Imagem</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {livros.map(livro => {
-                                return (
-                                    <tr key={livro.id}>
-                                        <td>{livro.titulo}</td>
-                                        <td>{livro.autor}</td>
-                                        <td>{livro.categoria}</td>
-                                        <td>{livro.isbn}</td>
-                                        <td>
-                                            <img src={livro.urlCapa} alt={livro.titulo} />
-                                        </td>
-                                        <td>
-                                            <Button 
-                                                as={Link}
-                                                to={`/livros/editar/${livro.id}`}
-                                                variant="warning"
-                                                size="sm"
-                                                className="me-2"
-                                                
-                                            >
-                                                <i className="bi bi-pencil-fill "></i>
-                                            </Button>
-                                        
-                                            <Button size="sm" variant="danger" onClick={() => onDeleteLivro(livro.id, livro.titulo)}>
-                                                <i className="bi bi-trash3-fill "></i>
-                                            </Button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </Table>
-                }
-            </Container>
+  return (
+    <div className={temaEscuro ? "bg-dark text-light" : "bg-white text-dark"}>
+    <div className="livros">
+      <Container>
+        <div className="d-flex justify-content-between align-items-center">
+          <h1>Livros</h1>
+          <Button
+            as={Link}
+            to="/livros/adicionar"
+            style={{ backgroundColor: "#248dad" }}
+          >
+            Adicionar Livro
+          </Button>
         </div>
-    )
+        <hr />
+        {livros === null ? (
+          <Loader />
+        ) : (
+          <Table className="mb-0" bordered>
+            <thead>
+              <tr className={temaEscuro ? "bg-dark text-light" : "bg-white text-dark"}>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>Categoria</th>
+                <th>ISBN</th>
+                <th>Imagem</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody className={temaEscuro ? "bg-dark text-light" : "bg-white text-dark"}>
+              {livros.map((livro) => {
+                return (
+                  <tr key={livro.id}>
+                    <td>{livro.titulo}</td>
+                    <td>{livro.autor}</td>
+                    <td>{livro.categoria}</td>
+                    <td>{livro.isbn}</td>
+                    <td>
+                      <img src={livro.urlCapa} alt={livro.titulo} />
+                    </td>
+                    <td>
+                      <Button
+                        as={Link}
+                        to={`/livros/editar/${livro.id}`}
+                        variant="warning"
+                        size="sm"
+                        className="me-2"
+                      >
+                        <i className="bi bi-pencil-fill "></i>
+                      </Button>
+
+                      <Button
+                        size="sm"
+                        variant="danger"
+                        onClick={() => onDeleteLivro(livro.id, livro.titulo)}
+                      >
+                        <i className="bi bi-trash3-fill "></i>
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        )}
+      </Container>
+
+    </div>
+    </div>
+  );
 }
